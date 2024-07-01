af60adc305ef5da109016924778d74cbb1a1932d
leVert <selmamathis15@gmail.com>
2024-07-01 15:21:23 +0300

    'tests

---
+++ compile.js
@@ -0,0 +1,39 @@
+const path = require('path');
+const fs = require('fs');
+const hardhat = require('hardhat');
+
+async function main() {
+    try {
+        // Compile the contracts using Hardhat
+        await hardhat.run('compile');
+
+        // Directory where Hardhat stores compiled artifacts
+        const artifactsDir = path.join(__dirname, 'artifacts', 'contracts', 'Inbox.sol');
+
+        // Specify the contract name
+        const contractName = 'Inbox';
+        const artifactPath = path.join(artifactsDir, `${contractName}.json`);
+
+        // Check if the artifact file exists
+        if (fs.existsSync(artifactPath)) {
+            // Read the artifact file
+            const artifact = JSON.parse(fs.readFileSync(artifactPath, 'utf8'));
+            console.log('ABI and bytecode of Inbox:');
+            console.log('ABI:', artifact.abi);
+            console.log('Bytecode:', artifact.bytecode);
+
+            // Return ABI and bytecode
+            return {
+                abi: artifact.abi,
+                bytecode: artifact.bytecode
+            };
+        } else {
+            throw new Error(`Artifact file not found at path: ${artifactPath}`);
+        }
+    } catch (error) {
+        console.error(error);
+        process.exitCode = 1;
+    }
+}
+
+module.exports = main();
--- test/inbox.js
+++ test/inbox.js
@@ -1,30 +1,43 @@
-const assert=require('assert');
-const { Web3 } = require('web3'); //correctr way of importing this library
-const {ethers} = require('hardhat');
-const web3 = new Web3(ethers.provider);
+const assert = require('assert');
+const {Web3} = require('web3');
+const ganache = require('ganache');
+const { abi, bytecode } = require('../build/Inbox.json');
 
 
-class Car {
-  park(){
-    return 'stopped';
-  }
+const web3 = new Web3(ganache.provider());
 
-  drive(){
-    return 'vrooom';
-  }
-}
+let inbox;
+let Accounts;
 
-let car; //defining car ahead of time
-beforeEach(()=>{
-  car=new Car(); //duplicate should have a helper beforeEach
+beforeEach(async () => {
+  // Get list of all accounts
+  Accounts = await web3.eth.getAccounts();
+  
+  // Use one account to deploy the contract
+  inbox = await new web3.eth.Contract(abi)
+    .deploy({ data: bytecode, arguments: ['Hi there!'] })
+    .send({ from: Accounts[0], gas: '1000000' });
 });
-// describe for nesting
-describe ('Car',()=>{
-  it('can park', ()=>{ //no intrinsic link between the cars
-    assert.equal(car.park(), 'stopped');
+
+describe('Inbox', () => {
+  it('deploys a contract', () => { //simple deployment test using address option
+    assert.ok(inbox.options.address); //ok asks it it a truthy value then test passes
+    console.log(inbox);
+    console.log(Accounts)
   });
 
-  it('can drive', ()=>{
-    assert.equal(car.drive(), 'vrooom');
+  it('has a default message', async ()=>{
+    const message = await inbox.methods.message().call();
+    assert.equal(message, 'Hi there!');
   });
-}); 
+  //method in inbox.methods is an object containing all public methods in our contract
+
+  // Add more tests as needed
+
+  it('can change the message', async()=>{
+    await inbox.methods.setMessage('bye').send({from: Accounts[0]});
+    const message = await inbox.methods.message().call();
+    assert.equal(message, 'bye');
+    // test fails if that method isn't marked as async
+  })
+});
